fc25877d6f1431a0be57f657c943976ef1b13690
Maria Balakleeva <rose.masha@yandex.ru>
2021-06-03 14:43:39 +0300

    Some help

--- script.js
+++ script.js
@@ -5,20 +5,55 @@ const tasks = document.getElementsByClassName('task');
 const clearBtn = document.querySelector('.clear');
 const deleteBtns = document.getElementsByClassName('delete');
 
-const tasksArr = ['First', 'Second'];
+const tasksArr = ['First1', 'Second1'];
+
+const arr = [
+    {
+        name: 'First',
+        isDone: false
+    },
+    {
+        name: 'Second',
+        isDone: true
+    }
+]
 
 function renderTasks(arr) {
     tasksContainer.innerHTML = '';
     const html = arr
         .map((task) => {
-            return `<li class="task">
-        <span>${task}</span>
-        <button class="delete">X</button>
-    </li>        `;
+            const li = document.createElement('li')
+            li.className = 'task'
+
+            const span = document.createElement('span')
+            span.innerHTML = task
+
+            const deleteButton = document.createElement('button')
+
+            deleteButton.addEventListener('click',() => {
+                tasksArr = tasksArr.filter((item) => item !== task)
+                console.log(`tasksArr`, tasksArr)
+            })
+
+            li.appendChild(span)
+            li.appendChild(deleteButton)
+            console.log(`li`, li)
+
+            return li.innerHTML
+
+            
+
+
+    //         return `<li class="task">
+    //     <span>${task}</span>
+    //     <button class="delete">X</button>
+    // </li>`;
         })
         .join('');
     tasksContainer.insertAdjacentHTML('beforeend', html);
 }
+
+renderTasks(tasksArr)
 
 addBtn.addEventListener('click', () => {
     if (newTask.value) {
@@ -27,6 +62,8 @@ addBtn.addEventListener('click', () => {
         renderTasks(tasksArr);
     }
 });
+
+console.log(`deleteBtns`, deleteBtns)
 
 for (let btn of deleteBtns) {
     btn.addEventListener('click', () => {
